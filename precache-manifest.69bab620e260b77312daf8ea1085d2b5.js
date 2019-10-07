34f439a60d9bfac8b69a4eb3cbd973a9c07a524a
Daniel Pruitt <danielpruitt6@gmail.com>
2019-10-06 20:09:36 -0400

    Updates

---
+++ precache-manifest.69bab620e260b77312daf8ea1085d2b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "750686d23fd343099910e37ef0fc2d0f",
+    "url": "//Portfolio2019/index.html"
+  },
+  {
+    "revision": "7eda83339b18b8d3b09c",
+    "url": "//Portfolio2019/static/css/main.2c818151.chunk.css"
+  },
+  {
+    "revision": "9fbe8d1f1741cd59b39f",
+    "url": "//Portfolio2019/static/js/2.fd3733ce.chunk.js"
+  },
+  {
+    "revision": "7eda83339b18b8d3b09c",
+    "url": "//Portfolio2019/static/js/main.1083dd2b.chunk.js"
+  },
+  {
+    "revision": "c95b67e1d049ec296f25",
+    "url": "//Portfolio2019/static/js/runtime-main.2f3d616f.js"
+  },
+  {
+    "revision": "0e65adcddc139da6988500252c1b56c0",
+    "url": "//Portfolio2019/static/media/Azonix.0e65adcd.ttf"
+  }
+]);
